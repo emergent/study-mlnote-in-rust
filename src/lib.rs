1b313c3a5219fa15adf617892d199f6c211400c0
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2022-09-05 01:47:52 +0900

    impl simple & multiple regression

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use smartcore::linalg::naive::dense_matrix::*;
+
+pub fn slope_and_intercept(data: &[(f32, f32)]) -> (f32, f32) {
+    let len = data.len() as f32;
+    let x_avg = data.iter().map(|d| d.0).sum::<f32>() / len;
+    let y_avg = data.iter().map(|d| d.1).sum::<f32>() / len;
+
+    let cov_xy = data
+        .iter()
+        .map(|&(x, y)| (x - x_avg) * (y - y_avg))
+        .sum::<f32>()
+        / len;
+    let var_x = data
+        .iter()
+        .map(|&(x, _)| (x - x_avg) * (x - x_avg))
+        .sum::<f32>()
+        / len;
+
+    let a = cov_xy / var_x;
+    (a, y_avg - a * x_avg)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn set_degree(x: f32, deg: usize) -> Vec<f32> {
+    let mut v = vec![x];
+    for _ in 1..deg {
+        v.push(v[v.len() - 1] * x);
     }
+    v
+}
+
+pub fn format_legend(
+    deg: usize,
+    coef: &DenseMatrix<f32>,
+    intercept: f32,
+) -> String {
+    let mut s = String::new();
+    s = format!("{}{}", s, intercept);
+    for i in 0..deg {
+        s = format!("{} + {}x^{}", s, coef.get(i, 0), i + 1);
+    }
+    s
 }
