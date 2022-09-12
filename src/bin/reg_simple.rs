fd408ddff253b0e4974550507154b240cffc5603
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2022-09-12 11:17:36 +0900

    tweak

--- src/bin/reg_simple.rs
+++ src/bin/reg_simple.rs
@@ -21,23 +21,3 @@ fn run<P: AsRef<Path>>(path: P) -> Result<()> {
 
     Ok(())
 }
-
-fn slope_and_intercept(data: &[(f32, f32)]) -> (f32, f32) {
-    let len = data.len() as f32;
-    let x_avg = data.iter().map(|d| d.0).sum::<f32>() / len;
-    let y_avg = data.iter().map(|d| d.1).sum::<f32>() / len;
-
-    let cov_xy = data
-        .iter()
-        .map(|&(x, y)| (x - x_avg) * (y - y_avg))
-        .sum::<f32>()
-        / len;
-    let var_x = data
-        .iter()
-        .map(|&(x, _)| (x - x_avg) * (x - x_avg))
-        .sum::<f32>()
-        / len;
-
-    let a = cov_xy / var_x;
-    (a, y_avg - a * x_avg)
-}
